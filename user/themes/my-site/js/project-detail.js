63dd5638f9d0beb8e3e294caaa1a0856bdd096f3
edfranks84 <edfranks84@gmail.com>
2018-08-02 00:23:28 +0100

    change animations to rellax and make home more clickable

--- user/themes/my-site/js/project-detail.js
+++ user/themes/my-site/js/project-detail.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
-    var rellax = new Rellax('.rellax');
+    // var rellax = new Rellax('.rellax');
 
 });
 
 
 $(window).on("load resize", function(e) {
     if ($(window).width() > 820) {
-
+        var rellax = new Rellax('.rellax');
         // $('.inner-pages__wrap').enllax();
         // $('.responsive-top').enllax();
         // $('.responsive-bottom').enllax();
@@ -26,16 +26,16 @@ $(window).on("load resize", function(e) {
             //     duration: window.innerHeight
             // }).setTween(tweenTopImage).addTo(controller);
 
-            var tweenInner2 = new TimelineMax()
-                .add([
-                    TweenMax.fromTo(".project-inner__image:nth-child(3)", 1, { y: 100 }, { y: -200, ease: Linear.easeNone, force3D: true })
-                ]);
+            // var tweenInner2 = new TimelineMax()
+            //     .add([
+            //         TweenMax.fromTo(".project-inner__image:nth-child(3)", 1, { y: 100 }, { y: -200, ease: Linear.easeNone, force3D: true })
+            //     ]);
 
-            new ScrollMagic.Scene({
-                triggerElement: "#inner-pages",
-                triggerHook: "onEnter",
-                duration: window.innerHeight * 2.5
-            }).setTween(tweenInner2).addTo(controller);
+            // new ScrollMagic.Scene({
+            //     triggerElement: "#inner-pages",
+            //     triggerHook: "onEnter",
+            //     duration: window.innerHeight * 2.5
+            // }).setTween(tweenInner2).addTo(controller);
 
             // var tweenMobileHome = new TimelineMax()
             //     .add([
@@ -48,39 +48,39 @@ $(window).on("load resize", function(e) {
             //     duration: window.innerHeight * 2
             // }).setTween(tweenMobileHome).addTo(controller);
 
-            var tweenResponsiveTablet = new TimelineMax()
-                .add([
-                    TweenMax.fromTo(".tablet", 1, { y: 200 }, { y: -500, ease: Linear.easeNone, force3D: true })
-                ]);
-            new ScrollMagic.Scene({
-                triggerElement: ".responsive-project",
-                triggerHook: "onEnter",
-                duration: window.innerHeight * 3,
-                // duration: 500,
-                // offset: 75
-            }).setTween(tweenResponsiveTablet).addTo(controller);
+            // var tweenResponsiveTablet = new TimelineMax()
+            //     .add([
+            //         TweenMax.fromTo(".tablet", 1, { y: 200 }, { y: -500, ease: Linear.easeNone, force3D: true })
+            //     ]);
+            // new ScrollMagic.Scene({
+            //     triggerElement: ".responsive-project",
+            //     triggerHook: "onEnter",
+            //     duration: window.innerHeight * 3,
+            //     // duration: 500,
+            //     // offset: 75
+            // }).setTween(tweenResponsiveTablet).addTo(controller);
 
-            var tweenResponsiveMobile1 = new TimelineMax()
-                .add([
-                    TweenMax.fromTo(".responsive-mobile:nth-child(1)", 1, { y: 30 }, { y: -50, ease: Linear.easeNone, force3D: true })
-                ]);
+            // var tweenResponsiveMobile1 = new TimelineMax()
+            //     .add([
+            //         TweenMax.fromTo(".responsive-mobile:nth-child(1)", 1, { y: 30 }, { y: -50, ease: Linear.easeNone, force3D: true })
+            //     ]);
 
-            new ScrollMagic.Scene({
-                triggerElement: ".responsive-bottom",
-                triggerHook: "onEnter",
-                duration: window.innerHeight * 1.5
-            }).setTween(tweenResponsiveMobile1).addTo(controller);
+            // new ScrollMagic.Scene({
+            //     triggerElement: ".responsive-bottom",
+            //     triggerHook: "onEnter",
+            //     duration: window.innerHeight * 1.5
+            // }).setTween(tweenResponsiveMobile1).addTo(controller);
 
-            var tweenResponsiveMobile2 = new TimelineMax()
-                .add([
-                    TweenMax.fromTo(".responsive-mobile:nth-child(2)", 1, { y: 100 }, { y: -350, ease: Linear.easeNone, force3D: true })
-                ]);
+            // var tweenResponsiveMobile2 = new TimelineMax()
+            //     .add([
+            //         TweenMax.fromTo(".responsive-mobile:nth-child(2)", 1, { y: 100 }, { y: -350, ease: Linear.easeNone, force3D: true })
+            //     ]);
 
-            new ScrollMagic.Scene({
-                triggerElement: ".responsive-bottom",
-                triggerHook: "onEnter",
-                duration: window.innerHeight * 2
-            }).setTween(tweenResponsiveMobile2).addTo(controller);
+            // new ScrollMagic.Scene({
+            //     triggerElement: ".responsive-bottom",
+            //     triggerHook: "onEnter",
+            //     duration: window.innerHeight * 2
+            // }).setTween(tweenResponsiveMobile2).addTo(controller);
 
             var tweenResponsiveMobile3 = new TimelineMax()
                 .add([
